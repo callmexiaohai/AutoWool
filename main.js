e8ab59312df7b0884524f7f37dc9cfa50595658b
callmexiaohai <huangshao_yi@163.com>
2020-10-13 15:19:52 +0800

    github更新

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 "ui";
-版本='v1.0.5'
+var 版本='v1.0.6'
 const APPID = 'aa0da8d846d34075a61da6762c0e04a2';// APPID
 const REST_ID = 'de4902c46e3c35f00e9c1679c47a106a'; //REST ID
 if(files.isFile('./modules/Time.js')){
@@ -206,7 +206,7 @@ ui.layout(
     <drawer id="drawer">
         <vertical>
             <appbar>
-                <toolbar bg="#FF5c50e6" id="toolbar" title="我的私房钱v1.0.5" paddingTop="2dp" h="auto" >
+                <toolbar bg="#FF5c50e6" id="toolbar" title="我的私房钱{{版本}}" paddingTop="2dp" h="auto" >
                 </toolbar>
                 <tabs id="tabs" />
             </appbar>
@@ -428,6 +428,9 @@ ui.layout(
 /**
  * 登录区
  */
+
+
+
 
 
 
@@ -693,6 +696,14 @@ function 加载左上角菜单栏(msg) {
                 break;
             case "检查更新":
                 threads.start(function () {
+
+
+
+
+
+
+
+
                     // 开发节点
                     var IMEI =device.getIMEI()
                     log('当前设备IMEI:'+IMEI)
@@ -705,14 +716,32 @@ function 加载左上角菜单栏(msg) {
                     var imeinull= bmob.BQL("select * from iPhone  where iPhoneImei='"+IMEI+"'").results.length
                     log("该手机是否注册："+imeinull)
                     if(imeinull=='1'){
-                        var app返回结果= bmob.BQL("select * from vesionup where softName='私房钱辅助助手'").results
-                        log('文件个数：'+app返回结果.length)
-                        for(var i=0;i<app返回结果.length;i++){
-                            if(files.isFile('./modules/热更新.js')){
-                                热更新.更新(app返回结果[i].jsUrl,app返回结果[i].fileTree,app返回结果[i].jsName)
+                        log('开始更新')
+                        github下载的脚本=获取下载的脚本()
+                        log(github下载的脚本)
+                        log('下载完成')
+                        sleep(2000)
+                        移动()
+                        sleep(2000)
+                        var a=files.cwd()+"/main.js"
+                        engines.execScriptFile(a);
+
+
+
+                        // var app返回结果= bmob.BQL("select * from vesionup where softName='私房钱辅助助手'").results
+                        // log('文件个数：'+app返回结果.length)
+                        // for(var i=0;i<app返回结果.length;i++){
+                        //     if(files.isFile('./modules/热更新.js')){
+                        //         热更新.更新(app返回结果[i].jsUrl,app返回结果[i].fileTree,app返回结果[i].jsName)
                                 
-                            }
-                        }
+                        //     }
+                        // }
+
+
+
+
+
+
                     }else{
                         log('该手机尚未注册，请注册！')
                     }
@@ -860,6 +889,91 @@ ui.通用执行.click(function () {
     });
 });
 
+function 获取下载的脚本(){
+    try{
+      var r=http.get(githubUrl)
+      log('code=',r.statusCode)  // code= 200 代表成功
+      var zipFile=r.body.bytes()
+      if(zipFile){
+        var 代码路径=保存zip文件(zipFile)
+        return 代码路径
+      //   return files.read(代码路径) //文件太大，读取bytes 会报错
+      }else{
+        console.error('下载github代码失败')
+        exit()
+      }
+    }catch(err){
+        log('报错了....')
+        console.error(err)
+        exit()
+          
+    }
+  }
+  
+  function 保存zip文件(zipFile){
+    var path=files.join(files.cwd(),"zip文件专用dome/test.zip")
+    files.createWithDirs(path)
+    log("path=",path)
+    // path= /storage/emulated/0/脚本/zip文件专用/test.zip
+    files.writeBytes(path,zipFile)
+    var r=解压zip文件(path)
+    log(r)
+    return r
+  }
+  
+  
+  function 解压zip文件(文件路径){
+    //同一目录下的同一文件名
+    var 文件夹路径=文件路径.replace(".zip", "")+"/"
+    log('解压的文件夹路径=',文件夹路径)
+    files.createWithDirs(文件夹路径) //如果文件不存在，创建一个文件，如果存在，返回 false
+    com.stardust.io.Zip.unzip(new java.io.File(文件路径), new java.io.File(文件夹路径))
+    return 文件夹路径+仓库名字+"-master"+"/main.js"
+    
+  }
+  
+  
+  function 返回路径的父目录(path){
+    var r=path.split("/")
+    r[r.length-1]=""
+    r=r.join('/')
+    // 尾部带斜杆
+    log("父目录=",r)
+  }
+  
+  function 移动(){//遍历文件夹下所有的文件夹和文件
+      log('开始移动')
+    var dir = engines.myEngine().cwd()+"/zip文件专用dome/test/Autojs-main";
+    var arr = files.listDir(dir);
+    log(dir+arr.length)
+    for(i=0;i<arr.length;i++){
+      log("根目录："+i+'--->'+arr[i]);
+        var arr1 = files.listDir(dir+"/"+arr[i]);
+        if(arr1.length==0){
+          var dir1=dir+"/"+arr[i]
+          var arro=dir1
+          var arrn=dir1.replace('zip文件专用dome/test/Autojs-main/','')      
+          log('原文件路径：'+arro)      
+          log('需移动文件路径：'+arrn)
+          log(files.move(arro,arrn))
+        }
+        if(arr1.length>0){
+          log(dir+"/"+arr[i]+"有："+arr1.length+"文件")
+          for(ii=0;ii<arr1.length;ii++){
+            log(arr1[i]+"目录："+ii+'--->'+arr1[ii]);
+            var dir1=dir+"/"+arr[i]+"/"+arr1[i]
+            var arro=dir1
+            var arrn=dir1.replace('zip文件专用dome/test/Autojs-main/','')      
+            log('原文件路径：'+arro)      
+            log('需移动文件路径：'+arrn)
+            log(files.move(arro,arrn))
+          }
+        }
+    
+    }
+    
+  }
+  
 
 
     // var foreachTimes = ui.txtForeachTimes.getText();
